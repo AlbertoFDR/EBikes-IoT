946a46df77f846ba01793ffa35db3aeaab4f6fe1
AlbertoFDR <albertofdr@gmail.com>
2019-12-04 12:55:33 +0100

    Corlysis storage works fine now

--- ebikes/utils/storage.py
+++ ebikes/utils/storage.py
@@ -4,11 +4,10 @@ import requests
 def save_corlysis(temperature, humidity, loudness, gases):
     url = "https://corlysis.com:8086/write"
     params = {"db": "ebikes-iot", "u": "token", "p": "15b6a2088bbd8f1d3ed3449f4495c394"}
-    payload_temp = "Temperature,type=celsius value=" + temperature + "\n"
-    payload_hum = "Humidity,type=percentage value=" + humidity + "\n"
-    payload_loud = "Loudness,type=loudness value=" + loudness + "\n"
-    payload_gas = "Gases,type=gas value=" + gases + "\n"
+    payload_temp = "Temperature,type=celsius value=" + str(temperature) + "\n"
+    payload_hum = "Humidity,type=percentage value=" + str(humidity) + "\n"
+    payload_loud = "Loudness,type=loudness value=" + str(loudness) + "\n"
+    payload_gas = "Gases,type=gas value=" + str(gases) + "\n"
     payload = payload_temp + payload_hum + payload_loud + payload_gas
     requests.post(url, params=params, data=payload)
 
-save_corlysis("32", "30", "30", "30")
