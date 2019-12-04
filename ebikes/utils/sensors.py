7ca6f21cbe00ca57febb26e509cb8029b02e9155
albertofdr <albertofdr@pandora>
2019-12-04 12:08:46 +0100

    Add loudness class to utils

---
+++ ebikes/utils/sensors.py
@@ -0,0 +1,16 @@
+import math
+import sys
+import time
+from grove.adc import ADC
+
+
+class GroveLoudnessSensor:
+
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+
+    @property
+    def value(self):
+        return self.adc.read(self.channel)
+
