a009c585056aaa0123b9707d2223e9519e0f114e
AlbertoFDR <albertofdr@gmail.com>
2019-12-06 18:43:48 +0100

    Added in the server_main a While to read messages all the time

--- ebikes/server_main.py
+++ ebikes/server_main.py
@@ -4,11 +4,12 @@ from utils.storage import save_corlysis
 if __name__ == "__main__":
     print("==================== CLIENT EBIKES ===================== \n")
     lora = LoraEndpoint()
-    print("Waiting to receive a message... \n")
-    msg = lora.read()
-    print("Msg received! \n")
-    print("Storage in Corlysis! \n")
-    #Data to save temperature, humidity, loudness, gases, latitude, longitude
-    #save_corlysis(msg)
-    print("Data saved \n") 
+    while True:
+        print("Waiting to receive a message... \n")
+        msg = lora.read()
+        print("Msg received! \n")
+        print("Storage in Corlysis! \n")
+        #Data to save temperature, humidity, loudness, gases, latitude, longitude
+        #save_corlysis(msg)
+        print("Data saved \n") 
 
