cca844fd12f82d66600e122ed1a45ef6c7d55add
Aratz M. Lasa <aratzml@opendeusto.es>
2019-12-11 12:37:05 +0100

    Improved prints

--- client_main.py
+++ client_main.py
@@ -15,24 +15,24 @@ if __name__ == "__main__":
     lora_endpoint = lora.LoraEndpoint()
     print("Lora [OK]")
     weather_sensor = sensors.WeatherSensor(dht_type=WEATHER_SENSOR_DHT, pin=WEATHER_SENSOR_PIN)
-    print("Weather sensor [OK]")
+    print("Weather sensor ready [OK]")
     gas_sensor = sensors.GasSensorMQ2(GAS_SENSOR_CHANNEL)
-    print("Gas sensor [OK]")
+    print("Gas sensor ready [OK]")
     loudness_sensor = sensors.LoudnessSensor(LOUDNESS_SENSOR_CHANEEL)
-    print("Loudness sensor [OK]")
+    print("Loudness sensor ready [OK]")
 
     while True:
         print("Getting data from sensors...")
         humidity, temperature = weather_sensor.value
-        print(f"Humidty: {humidity} [OK]")
-        print(f"Temperature: {temperature} [OK]")
+        print(f"Humidty: {humidity}")
+        print(f"Temperature: {temperature}")
         gas = gas_sensor.value
-        print(f"Gas: {gas} [OK]")
+        print(f"Gas: {gas}")
         loudness = loudness_sensor.value
-        print(f"Loudness: {loudness} [OK]")
+        print(f"Loudness: {loudness}")
 
         gps = None  # todo: get gps data (lon,lat)
-        print(f"GPS: {gps} [OK]")
+        print(f"GPS: {gps}")
         print("All data got from sensors [OK]")
         sensor_data = {
             prot.HUMIDITY_FIELD: humidity,
@@ -42,8 +42,10 @@ if __name__ == "__main__":
             prot.GPS_FIELD: gps
         }
         encoded_data = prot.dump_sensor_data(sensor_data)
+        print(f"JSON data encoded: {encoded_data}")
         print(f"Sending data to Lora Endpoint...")
         lora_endpoint.write(encoded_data)
         print(f"Data sent [OK]")
 
+        print(f"Sleeping {SAMPLING_FREQUENCY} seconds...")
         time.sleep(SAMPLING_FREQUENCY)
