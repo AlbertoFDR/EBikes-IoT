de5473e61b53cc5d08a81d6873882dccf2f527c1
Aratz M. Lasa <aratzml@opendeusto.es>
2019-12-11 12:33:36 +0100

    Added prints to main

--- client_main.py
+++ client_main.py
@@ -13,18 +13,27 @@ LOUDNESS_SENSOR_CHANEEL = 2
 
 if __name__ == "__main__":
     lora_endpoint = lora.LoraEndpoint()
-
+    print("Lora [OK]")
     weather_sensor = sensors.WeatherSensor(dht_type=WEATHER_SENSOR_DHT, pin=WEATHER_SENSOR_PIN)
+    print("Weather sensor [OK]")
     gas_sensor = sensors.GasSensorMQ2(GAS_SENSOR_CHANNEL)
+    print("Gas sensor [OK]")
     loudness_sensor = sensors.LoudnessSensor(LOUDNESS_SENSOR_CHANEEL)
+    print("Loudness sensor [OK]")
 
     while True:
+        print("Getting data from sensors...")
         humidity, temperature = weather_sensor.value
+        print(f"Humidty: {humidity} [OK]")
+        print(f"Temperature: {temperature} [OK]")
         gas = gas_sensor.value
+        print(f"Gas: {gas} [OK]")
         loudness = loudness_sensor.value
+        print(f"Loudness: {loudness} [OK]")
 
         gps = None  # todo: get gps data (lon,lat)
-
+        print(f"GPS: {gps} [OK]")
+        print("All data got from sensors [OK]")
         sensor_data = {
             prot.HUMIDITY_FIELD: humidity,
             prot.TEMPERATURE_FIELD: temperature,
@@ -33,6 +42,8 @@ if __name__ == "__main__":
             prot.GPS_FIELD: gps
         }
         encoded_data = prot.dump_sensor_data(sensor_data)
+        print(f"Sending data to Lora Endpoint...")
         lora_endpoint.write(encoded_data)
+        print(f"Data sent [OK]")
 
         time.sleep(SAMPLING_FREQUENCY)
