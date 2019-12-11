8e1159b9f438204193fc9c098909471449a4d40e
Aratz M. Lasa <aratzml@opendeusto.es>
2019-12-11 12:50:36 +0100

    Improved prints

--- server_main.py
+++ server_main.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     print("==================== CLIENT EBIKES ===================== \n")
     lora = LoraEndpoint()
     while True:
-        print("Waiting to receive a message... \n")
+        print("Waiting to receive a message...")
         msg = lora.read()
-        print("Msg received! \n")
-        print("Storage in Corlysis! \n")
+        print(f"Msg received! {msg}")
+        print("Storage in Corlysis!")
         #Data to save temperature, humidity, loudness, gases, latitude, longitude
         #save_corlysis(msg)
-        print("Data saved \n") 
+        print("Data saved")
 
